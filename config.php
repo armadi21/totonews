eb80724c88bf88a9f855f1e91747cb16379a78fc
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-12-05 06:52:28 +0800

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config = [
-"imei" => "864217032437572",
-"sign" => "8612da90bca3d436cf397dd788cc04f586cdb2d6",
-"token" => "9235e403907de8418fb0e22ad25f99d1",
-"uuid" => "17def921-bf26-4bf2-bd36-17fbf1ce8c2e"
-];
