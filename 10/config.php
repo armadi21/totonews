40211266194e0f4fbf105baa4d81623033623499
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-12-09 00:59:04 +0800

    Add files via upload

---
+++ 10/config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "354730020111652",
+"sign" => "03e46187a160c10aa213aae506cf637958f4784f",
+"token" => "181b26eaec6a27c38dcfc6f2b8894655",
+"uuid" => "4f9b050f-91ef-4052-91a6-9e170c5d454b"
+];
---
+++ 9/config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "354730020111112",
+"sign" => "a2f6dee310e3310ce8334f21f2b0b6b1a92eddbf",
+"token" => "7dce6a5fb7dbed8cddc491db0c49e6ac",
+"uuid" => "01d357f1-c8e9-498c-b8b5-450b05a113a4"
+];
