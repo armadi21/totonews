dbf4b21b6fea2f3fb79cee189cbc22539b3113ce
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-12-11 04:06:49 +0800

    Add files via upload

---
+++ 17/config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "354730020719112",
+"sign" => "23b2b32c82e14173d4fce24ca5770cbae3a9ab0e",
+"token" => "3a3ce557627bc729450dbadc9b288eff",
+"uuid" => "422e20ea-b412-479f-8e94-c6c46801e455"
+];
